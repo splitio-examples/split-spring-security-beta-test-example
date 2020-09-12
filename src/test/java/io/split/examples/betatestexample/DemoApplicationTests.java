8934bdc4e9a4f15451e3a17cfba13c6931693767
Micah Silverman <micah@afitnerd.com>
2020-09-12 13:21:34 -0400

    Initial Revision

---
+++ src/test/java/io/split/examples/betatestexample/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package io.split.examples.betatestexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
